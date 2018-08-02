577933f9fda620ce3a190d9164859d6da1dd2a7f
Erwin Müller <erwin.mueller@deventm.org>
2018-08-02 11:07:29 +0200

    See #4065 Update plugins

--- image/download_plugins.sh
+++ image/download_plugins.sh
@@ -2,19 +2,20 @@
 set -e
 
 plugins_urls=(
-https://downloads.wordpress.org/plugin/add-to-any.1.7.19.zip
-https://downloads.wordpress.org/plugin/akismet.4.0.zip
+https://downloads.wordpress.org/plugin/add-to-any.1.7.28.zip
+https://downloads.wordpress.org/plugin/akismet.4.0.8.zip
 https://downloads.wordpress.org/plugin/all-in-one-wp-security-and-firewall.zip
-https://downloads.wordpress.org/plugin/black-studio-tinymce-widget.2.5.1.zip
+https://downloads.wordpress.org/plugin/black-studio-tinymce-widget.2.6.2.zip
 https://downloads.wordpress.org/plugin/broken-link-checker.1.11.5.zip
-https://downloads.wordpress.org/plugin/cookie-notice.1.2.39.zip
-https://downloads.wordpress.org/plugin/si-contact-form.4.0.56.zip
+https://downloads.wordpress.org/plugin/contact-form-7.5.0.3.zip
+https://downloads.wordpress.org/plugin/cookie-notice.1.2.44.zip
 https://downloads.wordpress.org/plugin/favicon-by-realfavicongenerator.zip
-https://downloads.wordpress.org/plugin/the-events-calendar.4.6.1.zip
-https://downloads.wordpress.org/plugin/wp-piwik.zip
-https://downloads.wordpress.org/plugin/wp-super-cache.1.5.7.1.zip
-https://downloads.wordpress.org/plugin/wp-optimize.2.1.1.zip
-https://downloads.wordpress.org/plugin/wp-mail-smtp.0.10.1.zip
+https://downloads.wordpress.org/plugin/google-analytics-dashboard-for-wp.5.3.5.zip
+https://downloads.wordpress.org/plugin/post-smtp.zip
+https://downloads.wordpress.org/plugin/the-events-calendar.4.6.21.zip
+https://downloads.wordpress.org/plugin/wp-smushit.2.8.0.zip
+https://downloads.wordpress.org/plugin/wp-super-cache.1.6.2.zip
+https://downloads.wordpress.org/plugin/wp-optimize.2.2.4.zip
 )
 
 cd ..
@@ -34,7 +35,7 @@ for url in ${plugins_urls[@]}; do
   fi
   sha=$(sha256sum ${file} | cut -d' ' -f1)
   echo "RUN set -x \\" >> plugins_dockerfile.txt
-  echo "  && install_wp_plugin.sh ${url} '${sha}'" >> plugins_dockerfile.txt
+  echo "  && /install_wp_plugin.sh ${url} '${sha}'" >> plugins_dockerfile.txt
   echo "" >> plugins_dockerfile.txt
 done
 
